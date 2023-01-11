e3b1f34ca085b699b936a48284bcc83a8949b48e
Regina <gumerovars@gmail.com>
2023-01-11 21:09:22 +0500

    second task

---
+++ ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число");
+string a = Console.ReadLine();
+char[] array = a.ToCharArray();
+int sum = addition(array);
+Console.WriteLine("Сумма цифр = " + sum);
+
+
+int addition(char [] array)
+{
+    int length = array.Length;
+    int result = 0;
+    for (int i = 0; i < length; i++)
+    {
+        int c = int.Parse(array[i].ToString());
+        result = result + c;
+    }
+    return result;
+}
+
+
