ac0cdd5f62ab311f4cf4b3578f71be1e8971e8be
Regina <gumerovars@gmail.com>
2023-01-11 19:37:20 +0500

    corrected solution

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -2,11 +2,16 @@
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число B");
 int b = Convert.ToInt32(Console.ReadLine());
-double c = 1;
-
-for (int i = 0; i < b; i++)
-{
-    c = Math.Pow(a,b);
-}
-
+int c = exponentiation(a,b);
 Console.WriteLine("A^B=" + c);
+
+
+int exponentiation(int a, int b)
+{
+    int result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
