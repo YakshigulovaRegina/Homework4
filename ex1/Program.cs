8b54d52f5d6a3a54a70545ba8c57c260828a7f06
Regina <gumerovars@gmail.com>
2023-01-11 16:08:04 +0500

    first task

---
+++ ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+double c = 1;
+
+for (int i = 0; i < b; i++)
+{
+    c = Math.Pow(a,b);
+}
+
+Console.WriteLine("A^B=" + c);
