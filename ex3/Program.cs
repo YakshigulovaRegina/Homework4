783eb4a3485306399820c12d76e57dccbbe32191
Regina <gumerovars@gmail.com>
2023-01-12 17:46:22 +0500

    third task

---
+++ ex3/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = generateArray(8);
+PrintArray(array);
+
+int[] generateArray (int arraySize)
+{
+    int[] array = new int[arraySize];
+    for (int i = 0; i < arraySize; i++)
+    {
+        array[i] = new Random().Next(0,10);
+    }  
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
